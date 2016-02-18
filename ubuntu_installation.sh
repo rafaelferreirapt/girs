c76c485b8859ff8930bcf952def4e64caf8ebc9f
Rafael Ferreira <mail@rafaelferreira.pt>
2016-02-18 15:57:53 +0000

    offline repository

--- ubuntu_installation.sh
+++ ubuntu_installation.sh
@@ -1,10 +1,13 @@
+# we made an offline repository in one external disk
+# http://askubuntu.com/questions/974/how-can-i-install-software-or-packages-without-internet-offline
+# answer: A USB repository
 sudo su
 fdisk -l
 # Disk /dev/sdb: 500.1 GB
 mkdir /media/disk
 mount /dev/sdb /media/disk/
 mv /etc/apt/sources.list /etc/apt/sources.list.old
-echo "deb [arch=amd64] file:/media/disk/mirror/archive.ubuntu.com/ubuntu trusty main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb [arch=amd64] file:/media/disk/mirror/archive.ubuntu.com/ubuntu trusty main restricted universe multiverse" > /etc/apt/sources.list
 apt-get update
 apt-get install -y docker.io
 
