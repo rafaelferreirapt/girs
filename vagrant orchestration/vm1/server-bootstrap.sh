8259c009d0a0c2b2e79745d068983390bca2520c
Rafael Ferreira <mail@rafaelferreira.pt>
2016-02-25 22:12:28 +0000

    vagrant vms 2048 mb

---
+++ vagrant orchestration/vm1/server-bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo su
+locale-gen UTF-8
+apt-get update
+apt-get install -y apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get -y install docker-engine
+service docker start
+
+docker pull ubuntu
+sudo docker run hello-world
