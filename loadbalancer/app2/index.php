1e1265e2595a0e1d655df374b7148c82661fa919
Rodrigo Cunha <rodrigocunha@ua.pt>
2016-04-29 16:36:38 +0100

    index app random

---
+++ loadbalancer/app2/index.php
@@ -0,0 +1,20 @@
+<?php
+
+const INTERVAL = 0.04;
+$dummy = 0;
+$cycles = 100000;
+
+$rand = rand(10000, 100000);
+for($j = 0; $j < $rand; $j++){
+  $start = gettimeofday();
+  for($i=0; $i<$cycles; $i++){
+    $dummy += $i*$i;
+  }
+  $stop = gettimeofday();
+
+  $spentTime = ($stop["sec"]-$start["sec"]) + ($stop["usec"]-$start["usec"]) / 1000000.0;
+  $cycles = ($cycles / $spentTime) * ($target_load / 100.0) * INTERVAL;
+  usleep((1000000 * INTERVAL - $spentTime*1000000));
+}
+
+?>
